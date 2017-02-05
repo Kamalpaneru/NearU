5f6e3dc2a982c47a27b79ce4ce3d200e6659d85a
eroj333 <shresthaenosh35@yahoo.com>
2017-02-05 19:47:51 +0545

    Update constructor for Events and Company

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
-# import requests
-# import json
+import requests
+import json 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:3306/cb_core_db'
 db = SQLAlchemy(app)
-
 
 class Company(db.Model):
     __table__name = "Company"
@@ -16,14 +15,15 @@ class Company(db.Model):
     description = db.Column(db.Text)
     org_type = db.Column(db.String(20))
     logo = db.Column(db.String(100))
-
-    def __init__(self, name, location):
+    def __init__(self, name, location, description, org_type, logo):
         self.name = name
         self.location = location
+        self.description = description
+        self.org_type = org_type
+        self.logo = logo
 
     def __repr__(self):
         return '<Company %r>' % self.name
-
 
 class Events(db.Model):
     __tablename__ = "Events"
@@ -34,10 +34,13 @@ class Events(db.Model):
     description = db.Column(db.Text)
     organizer = db.Column(db.Integer, db.ForeignKey('company.id'))
     banner = db.Column(db.String(1000))
-
-    def __init__(self, title, location):
+    def __init__(self, title, location, dateTime, description, organizer, banner):
         self.title = title
         self.location = location
+        self.dateTime = dateTime
+        self.description = description
+        self.organizer = organizer
+        self.banner = banner
 
     def __repr__(self):
         return '<Event %r>' % self.title
@@ -50,11 +53,10 @@ class Reviews(db.Model):
     rating = db.Column(db.Float)
     eventID = db.Column(db.Integer, db.ForeignKey('Events.id'))
 
-    def __init__(self, comment, rating, eventID):
+    def __init__ (self, comment, rating , eventID):
         self.comment = comment
         self.rating = rating
         self.eventID = eventID
-
 
 class Landmark(db.Model):
     __tablename__ = "Landmarks"
@@ -63,21 +65,21 @@ class Landmark(db.Model):
     description = db.Column(db.Text)
     image = db.Column(db.String(1000))
 
-    def __init__(self, location, description, image):
+    def __init__ (self, location, description, image):
         self.location = location
         self.description = description
         self.image = image
 
-
 @app.route('/')
 def home():
     return render_template('index.html')
-
-
 @app.route('/response', methods=['POST'])
 def response():
-    # print requests;
-    return jsonify({'text': 'Check the map.'})
+    print requests;
+    return jsonify({'text': 'response will go here'})
+@app.route('/chatbox')
+def chatbox():
+    return render_template('chatbox.html')
 
 if __name__ == '__main__':
     print db
